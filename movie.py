0f8c786d51ec975b60f811279401d9efe58a6166
Maxime <maxilefranc@gmail.com>
2023-11-01 11:10:41 -0500

    definition of 2 news private functions: _get_movies() and _write_movies() in Movie class

--- movie.py
+++ movie.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+import json
+from pathlib import Path
+
+MOVIES_DB = Path.cwd() / 'data' / 'movies.json'
 
 
 @dataclass
@@ -11,8 +15,26 @@ class Movie:
     def __str__(self) -> str:
         return self.title
 
+    def _get_movies(self) -> list:
+        try:
+            with open(MOVIES_DB, 'r') as f:
+                content = json.load(f)
+        except json.decoder.JSONDecodeError:
+            print('Il n\'y a pas de films dans la base de donnée.')
+        else:
+            return content
+
+        return []
+
+    def _write_movies(self, movies):
+        with open(MOVIES_DB, 'w') as f:
+            json.dump(movies, f, indent=4)
+
 
 if __name__ == '__main__':
     movie = Movie('harry potter')
-    print(movie.title)
-    print(movie)
+    # print(movie.title)
+    # print(movie)
+    # print(MOVIES_DB)
+    # print(movie._write_movies(["Harry Potter", "Barry Lyndon"]))
+    print(movie._get_movies())
